168dff6b0bcfde035d268afb0ac3ec59d40c6ea6
罗飞 <luofei4@xdf.cn>
2017-08-12 20:54:02 +0800

    初始化项目

---
+++ TXVideoPod/TXVideoPod/TXVideoPod.h
@@ -0,0 +1,19 @@
+//
+//  TXVideoPod.h
+//  TXVideoPod
+//
+//  Created by 罗飞 on 12/08/2017.
+//  Copyright © 2017 cdone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TXVideoPod.
+FOUNDATION_EXPORT double TXVideoPodVersionNumber;
+
+//! Project version string for TXVideoPod.
+FOUNDATION_EXPORT const unsigned char TXVideoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TXVideoPod/PublicHeader.h>
+
+
---
+++ mqq/mqq/mqq.h
@@ -0,0 +1,19 @@
+//
+//  mqq.h
+//  mqq
+//
+//  Created by 罗飞 on 12/08/2017.
+//  Copyright © 2017 cdone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mqq.
+FOUNDATION_EXPORT double mqqVersionNumber;
+
+//! Project version string for mqq.
+FOUNDATION_EXPORT const unsigned char mqqVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mqq/PublicHeader.h>
+
+
